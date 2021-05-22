cc43efdb0f453fe1ecc2cea89b0344b9b224d7a6
Piyush Singhal <piyushlimited@gmail.com>
2021-05-22 08:48:30 +0530

    Weather Application Version 1.0 Completed.

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:weather_app/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:weather_app/services/weather.dart';
+const apiKey = 'dd4778c141c429e7deddd89722a90a63';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+      getLocationData();
+  }
+
+  void getLocationData() async {
+
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen(locationWeather: weatherData);
+    }
+    ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(
+            child: SpinKitDoubleBounce(
+              color: Colors.white,
+              size: 100.0,
+            )
+        ),
+      );
+
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const KTextFieldInputStyle = InputDecoration(
+    icon: Icon(
+      Icons.location_city,
+      color: Colors.white,
+    ),
+    filled: true,
+    fillColor: Colors.white,
+    hintText: 'Enter city name here.',
+    hintStyle: TextStyle(
+      color: Colors.grey,
+    ),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      borderSide: BorderSide.none,
+    ),
+);
